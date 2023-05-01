d1279b0cd10dc6036648e7a4d901f7de4915a501
brendan <brendan.whelan@sydney.edu.au>
2023-05-01 13:42:02 +1000

    update version handling to toml file

--- get_all_files/__init__.py
+++ get_all_files/__init__.py
@@ -1,3 +1,6 @@
-
-__version__ = '0.0.2'
+import importlib.metadata
+try:
+    __version__ = importlib.metadata.version("get_all_files")
+except:
+    pass
 from get_all_files._get_all_files import _get_all_files as get_all_files  # flake8: noqa
